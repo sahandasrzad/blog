6de45971c86820e3e48dc7952bf929b3af4d230d
asrzad <sahandasrzad@gmail.com>
2024-08-26 02:08:08 +0330

    fix issue in link to post/byId

--- src/lib/postApi.js
+++ src/lib/postApi.js
@@ -2,7 +2,7 @@
 
 // Fetch a single post by ID
 export async function fetchPostById(postId) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${postId}`, { cache: 'no-store' });
+    const res = await fetch(`/api/posts/${postId}`, { cache: 'no-store' });
     
     if (!res.ok) {
       const errorData = await res.json();
